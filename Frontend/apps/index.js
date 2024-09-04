ca2507fb093f864c4a5323035510228da0ff95ec
Brayan <jesus.brayancastillo15@gmail.com>
2024-09-04 09:14:11 -0500

    Elimina archivos obsoletos y actualiza archivos modificados

--- Frontend/apps/index.js
+++ Frontend/apps/index.js
@@ -1,127 +1,100 @@
-const api_url = 'http://localhost:5000/users'
+import { crearUsers, deleteUser, fetchUsers, updateUsers } from './api.mjs'
+import { validateForm } from './validation.mjs'
+document.getElementById('show-create-form-btn').addEventListener('click', () => {
+    document.getElementById('create-form-container').classList.remove('hidden');
+    document.getElementById('update-form-container').classList.add('hidden');
+});
+
+document.getElementById('show-update-form-btn').addEventListener('click', () => {
+    document.getElementById('update-form-container').classList.remove('hidden');
+    document.getElementById('create-form-container').classList.add('hidden');
+});
+
+//mostrar todos los usuarios
+
+async function mostrarUsers() {
+
+    //contenedor donde se mostraran los registros
+    const users = await fetchUsers()
+    const userList = document.getElementById('users-list')
+    userList.innerHTML = ''
+
+    users.forEach(element => {
+
+        const row = document.createElement('tr')
+        row.innerHTML =
+            `
+         <td>${element.id}</td>
+             <td>${element.name}</td>
+             <td>${element.email}</td>
+              <td>
+                        <button type="button" class="btn btn-warning btn-sm update-btn" data-id="${element.id}"> Actualizar</button>
+                        <button type="button" class="btn btn-danger btn-sm delete-btn" data-id="${element.id}">Eliminar</button>
 
 
-async function fetchUsers() {
-    try {
-        const response = await fetch(api_url)
-        const usersList = await response.json()
+                    </td>
+        `
+        userList.appendChild(row)
+        //Accion para eliminar registro de usuaros
+        row.querySelectorAll('.delete-btn').forEach(btn => {
+            btn.addEventListener('click', deleteUsers)
+        })
+        //Accion para actualizar registros de 
+        row.querySelectorAll('.update-btn').forEach(element => {
+            element.addEventListener('click', update)
+        })
 
-
-        const producstList = document.getElementById('products-list')
-
-        producstList.innerHTML = ''
-
-        usersList.forEach(user => {
-            const row = document.createElement('tr')
-            row.innerHTML = `
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>
-                <button class="btn btn-danger btn-sm" onclick="deleteUser(${user.id})">Eliminar</button>
-            </td>
-        `;
-            producstList.appendChild(row)
-
-
-        });
-
-    } catch (error) {
-        console.error('Error al obtener los productos', error);
-    }
+    });
 }
 
-//manejo de formulario para la creacion de usuairios
-document.getElementById('create-form').addEventListener('submit', async (event) => {
 
-    event.preventDefault()//pàra prevenir que la pagina se cargue
-    let name = document.getElementById('create-name').value
-    let email = document.getElementById('create-email').value
+//manejo de formulario para la creacion de un registro
+document.getElementById('create-form-container').addEventListener('submit', async (e) => {
 
-    console.log(name)
-    console.log(email)
-    try {
-        const response = await fetch(api_url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email })
-        })
-        const data = await response.json()
-        console.log('usurio creado ', data)
-        
+    e.preventDefault()
+    const nameusers = document.getElementById('create-name').value
+    const emailuser = document.getElementById('create-email').value
 
-    } catch (error) {
-        console.log('Error al intentar crear dato')
+    if (!validateForm(nameusers, emailuser)) return
+    const registro = await crearUsers(nameusers, emailuser)
+
+    if (registro) {
+        alert('Registro creado')
+        mostrarUsers()
     }
-})
-//manejo de formulario para actualizar datos de usuario
 
-document.getElementById('update-form').addEventListener('submit', async (event) => {
-    event.preventDefault()
-    const id = parseInt(document.getElementById('update-id').value)
+})
+
+//Manejo de formulario para la actualizacion de registro
+async function update(e) {
+    e.preventDefault()
+    const userID = e.target.getAttribute('data-id')
     const name = document.getElementById('update-name').value
     const email = document.getElementById('update-email').value
+    // Verifica el valor del email
+    if (!validateForm(name, email)) return
+    const result = await updateUsers(userID, name, email)
 
-
-    if (isNaN(id) || id <= 0) return
-    console.log(id)
-    console.log(name)
-    console.log(email)
-
-    try {
-        const response = await fetch(`${api_url}/${id}`, {
-            method: `PUT`,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email })
-        })
-        // Verifica si la respuesta fue exitosa (códigos 200-299)
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status} - ${response.statusText}`);
-        }
-        const data = await response.json()
-        console.log('Dato actualizado')
-        fetchUsers()
-    } catch (error) {
-        console.log(error)
-        console.log('Error al intentar al actualizar datos')
-
+    if (result) {
+        alert('Registro Actualizado')
+        mostrarUsers()
     }
 
-})
-
-
-//funcion para  eliminar datos
-async function deleteUser(id) {
-    try {
-        const response = await fetch(`${api_url}/${id}`,
-            {
-                method: 'DELETE',
-
-            }
-        )
-
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status} - ${response.statusText}`);
-        }
-        const data = await response.json()
-        console.log('dato eliminado')
-        fetchUsers()
-    } catch (error) {
-
-    }
 
 
 }
+//Manejo de formulario  para la eliminacion de registro
 
-//Boton par mostrar lista se usuarios
+async function deleteUsers(e) {
 
-let btnAbrir = document.getElementById('btn-abrir')
-btnAbrir.addEventListener('click', async (event) => {
+    e.preventDefault()
+    const userID = e.target.getAttribute('data-id')
+    const result = await deleteUser(userID)
+    if (result) {
+        alert('Registro eliminado')
 
-    event.preventDefault()
-    await fetchUsers()
-})
+        mostrarUsers()
+    }
+
+}
+mostrarUsers()
